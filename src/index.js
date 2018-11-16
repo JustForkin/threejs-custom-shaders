e4f926b2c02edce515c5e9a2e0e3b674e5b07a57
pfreema1 <pfreema1@gmail.com>
2018-11-15 19:36:50 -0500

    meow

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import ScrollCam from './app/ScrollCam.js';
 import PostEffects from './app/PostEffects.js';
 import Hallways from './app/Hallways.js';
 import {TweenMax, TimelineLite} from "gsap/TweenMax";
+import OrbitControls from 'orbit-controls-es6';
+
+let renderer, scene, controls, camera, material, pointLight, geometry, sphere, light = null;
+const container = document.getElementById('container');
+const clock = new THREE.Clock();
 
 document.addEventListener("DOMContentLoaded", () => {
-  let renderer, scene, mixer, scrollCam, postEffects, hallways = null;
-  const container = document.getElementById('container');
-  const clock = new THREE.Clock();
-  const modelUrl = './static/Hallway/hallwaylit.json';
 
   initialize().then( () => {
     // now we can kick off the animate/render loop
@@ -19,50 +20,70 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   async function initialize() {
+      scene = new THREE.Scene();
+      camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
       renderer = new THREE.WebGLRenderer({
         antialias: true,	// to get smoother output
       });
-      renderer.setClearColor( 0x3b3b3b );
-      renderer.shadowMap.enabled = true;
-      renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+      renderer.setClearColor( 0x1e1e1e );
       renderer.setSize( window.innerWidth, window.innerHeight );
       container.appendChild(renderer.domElement);
+      controls = new OrbitControls( camera, renderer.domElement );
+      camera.position.z = 50;
+      controls.enabled = true;
+      controls.update();
 
-      // import hallway object, and clone it a few times
-      hallways = new Hallways();
-      scene = await hallways.setupScene(modelUrl);
+      addGeometry();
 
-      // create our camera that listens to scrollz
-      scrollCam = new ScrollCam(scene,
-        new THREE.Vector3(0, -3, -5),
-      5, 0.05, 20, 0.5);
-
-      // have each hallway spawn neighboring hallways when user enters them
-      hallways.setupColliders(scrollCam);
-
-      // create some post effects to apply to our camera
-      postEffects = new PostEffects(scene, renderer, scrollCam.camera);
+      addLights();
+      
   }
 
   function animate() {
-    update();
     requestAnimationFrame( animate );
+
+    update();
+
+    controls.update();
+    renderer.render(scene, camera);
   }
+
+  var start = Date.now();
 
   function update() {
     // let all of our scriptz run their respective update loop
     var delta = clock.getDelta();
+    var elapsedTime = Date.now() - start;
 
-    if (mixer != null)
-      mixer.update(delta);
+    material.alphaMap.offset.y = elapsedTime * 0.0002;
 
-    if (scrollCam != null)
-      scrollCam.update(delta);
+  }
 
-    if (postEffects != null)
-      postEffects.update(delta);
+  function addGeometry() {
+    let alphaMap = new THREE.TextureLoader().load('../static/texture.png');
 
-    if (hallways != null)
-      hallways.update();
+    geometry = new THREE.SphereGeometry( 50, 50, 50 );
+    material = new THREE.MeshStandardMaterial( { 
+      color: "#000",
+      transparent: true,
+      side: THREE.DoubleSide, 
+      alphaTest: 0.5, 
+    } );
+    material.alphaMap = alphaMap;
+    material.alphaMap.magFilter = THREE.NearestFilter;
+    material.alphaMap.wrapT = THREE.RepeatWrapping;
+    material.alphaMap.repeat.y = 1;
+    sphere = new THREE.Mesh( geometry, material );
+    scene.add( sphere );
+
+  }
+
+  function addLights() {
+    light = new THREE.AmbientLight( 0x404040 ); // soft white light
+    scene.add( light );
+
+    pointLight = new THREE.PointLight(0xffffff);
+    pointLight.position.z = 0;
+    scene.add(pointLight);
   }
 });
