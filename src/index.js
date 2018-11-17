78f0b5767d17b1e0bddebe680863f4a57e1575fb
pfreema1 <pfreema1@gmail.com>
2018-11-16 19:54:46 -0500

    init

--- src/index.js
+++ src/index.js
@@ -9,147 +9,121 @@ import {TweenMax, TimelineLite} from "gsap/TweenMax";
 import OrbitControls from 'orbit-controls-es6';
 import PromisedLoad from './app/PromisedLoad';
 
-let renderer, scene, Josh, controls, camera, material, pointLight, geometry, sphere, light = null;
-let nMax;
-const container = document.getElementById('container');
-const clock = new THREE.Clock();
 
-document.addEventListener("DOMContentLoaded", () => {
 
-  initialize().then( () => {
-    // now we can kick off the animate/render loop
-    animate();
-  });
-
-  async function initialize() {
-      scene = new THREE.Scene();
-      camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-      renderer = new THREE.WebGLRenderer({
-        antialias: true,	// to get smoother output
-      });
-      renderer.setClearColor( 0x1e1e1e );
-      renderer.setSize( window.innerWidth, window.innerHeight );
-      container.appendChild(renderer.domElement);
-      controls = new OrbitControls( camera, renderer.domElement );
-      camera.position.z = 50;
-      controls.enabled = true;
-      controls.update();
-
-      PromisedLoad.GetGLTF('../static/josh.glb', JoshModelLoaded);
-
-      addSphere();
-
-      addLights();
-      
+var container;
+var camera, scene, renderer;
+var mesh;
+init();
+animate();
+function init() {
+  container = document.getElementById( 'container' );
+  //
+  camera = new THREE.PerspectiveCamera( 27, window.innerWidth / window.innerHeight, 1, 3500 );
+  camera.position.z = 2750;
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color( 0x050505 );
+  scene.fog = new THREE.Fog( 0x050505, 2000, 3500 );
+  //
+  scene.add( new THREE.AmbientLight( 0x444444 ) );
+  var light1 = new THREE.DirectionalLight( 0xffffff, 0.5 );
+  light1.position.set( 1, 1, 1 );
+  scene.add( light1 );
+  var light2 = new THREE.DirectionalLight( 0xffffff, 1.5 );
+  light2.position.set( 0, - 1, 0 );
+  scene.add( light2 );
+  //
+  var triangles = 160000;
+  var geometry = new THREE.BufferGeometry();
+  var positions = [];
+  var normals = [];
+  var colors = [];
+  var color = new THREE.Color();
+  var n = 800, n2 = n / 2;	// triangles spread in the cube
+  var d = 12, d2 = d / 2;	// individual triangle size
+  var pA = new THREE.Vector3();
+  var pB = new THREE.Vector3();
+  var pC = new THREE.Vector3();
+  var cb = new THREE.Vector3();
+  var ab = new THREE.Vector3();
+  for ( var i = 0; i < triangles; i ++ ) {
+    // positions
+    var x = Math.random() * n - n2;
+    var y = Math.random() * n - n2;
+    var z = Math.random() * n - n2;
+    var ax = x + Math.random() * d - d2;
+    var ay = y + Math.random() * d - d2;
+    var az = z + Math.random() * d - d2;
+    var bx = x + Math.random() * d - d2;
+    var by = y + Math.random() * d - d2;
+    var bz = z + Math.random() * d - d2;
+    var cx = x + Math.random() * d - d2;
+    var cy = y + Math.random() * d - d2;
+    var cz = z + Math.random() * d - d2;
+    positions.push( ax, ay, az );
+    positions.push( bx, by, bz );
+    positions.push( cx, cy, cz );
+    // flat face normals
+    pA.set( ax, ay, az );
+    pB.set( bx, by, bz );
+    pC.set( cx, cy, cz );
+    cb.subVectors( pC, pB );
+    ab.subVectors( pA, pB );
+    cb.cross( ab );
+    cb.normalize();
+    var nx = cb.x;
+    var ny = cb.y;
+    var nz = cb.z;
+    normals.push( nx, ny, nz );
+    normals.push( nx, ny, nz );
+    normals.push( nx, ny, nz );
+    // colors
+    var vx = ( x / n ) + 0.5;
+    var vy = ( y / n ) + 0.5;
+    var vz = ( z / n ) + 0.5;
+    color.setRGB( vx, vy, vz );
+    colors.push( color.r, color.g, color.b );
+    colors.push( color.r, color.g, color.b );
+    colors.push( color.r, color.g, color.b );
   }
-
-  function animate() {
-    requestAnimationFrame( animate );
-
-    update();
-
-    controls.update();
-    renderer.render(scene, camera);
+  function disposeArray() {
+    this.array = null;
   }
+  geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ).onUpload( disposeArray ) );
+  geometry.addAttribute( 'normal', new THREE.Float32BufferAttribute( normals, 3 ).onUpload( disposeArray ) );
+  geometry.addAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ).onUpload( disposeArray ) );
+  geometry.computeBoundingSphere();
+  var material = new THREE.MeshPhongMaterial( {
+    color: 0xaaaaaa, specular: 0xffffff, shininess: 250,
+    side: THREE.DoubleSide, vertexColors: THREE.VertexColors
+  } );
+  mesh = new THREE.Mesh( geometry, material );
+  scene.add( mesh );
+  //
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.gammaInput = true;
+  renderer.gammaOutput = true;
+  container.appendChild( renderer.domElement );
 
-  var start = Date.now();
+  //
+  window.addEventListener( 'resize', onWindowResize, false );
+}
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+//
+function animate() {
+  requestAnimationFrame( animate );
+  render();
 
-  function update() {
-    // let all of our scriptz run their respective update loop
-    var delta = clock.getDelta();
-    var elapsedTime = Date.now() - start;
-
-    // animate the sphere material
-    material.alphaMap.offset.y = elapsedTime * -0.00007;
-    // rotate the sphere
-    sphere.rotation.y += 0.01;
-
-    if(nMax != undefined && Josh) {
-      Josh.children[0].geometry.setDrawRange(0 * Math.abs(Math.cos(elapsedTime * 0.1)), nMax * Math.abs(Math.sin(elapsedTime * 0.1)));
-      
-    }
-    
-
-  }
-
-  function addSphere() {
-    let alphaMap = new THREE.TextureLoader().load('../static/texture.png');
-
-    geometry = new THREE.SphereBufferGeometry( 50, 32, 32, 0, 6.3, 5, 1.8 );
-    // geometry.drawRange.count = 2000;
-    material = new THREE.MeshStandardMaterial( { 
-      color: "#000",
-      transparent: true,
-      side: THREE.DoubleSide, 
-      alphaTest: 0.5, 
-    } );
-    material.alphaMap = alphaMap;
-    material.alphaMap.magFilter = THREE.NearestFilter;
-    material.alphaMap.wrapT = THREE.RepeatWrapping;
-    material.alphaMap.repeat.y = 1;
-    sphere = new THREE.Mesh( geometry, material );
-    scene.add( sphere );
-
-    //position at third eye
-    sphere.rotation.x += 90;
-    sphere.position.z -= 40;
-    sphere.position.y += 40;
-
-  }
-
-  function addLights() {
-    light = new THREE.AmbientLight( 0x404040 ); // soft white light
-    scene.add( light );
-
-    pointLight = new THREE.PointLight(0xffffff);
-    pointLight.position.z = 0;
-    scene.add(pointLight);
-  }
-
-  function JoshModelLoaded(importedObject) {
-    let alphaMap = new THREE.TextureLoader().load('../static/texture.png');
-    console.log('importedObject:  ', importedObject);
-    Josh = importedObject.scene.children[0];
-    console.log('Josh:  ', Josh);
-    scene.add(Josh);
-    Josh.position.set(0, 0, 0);
-    Josh.position.x -= 2;
-    Josh.rotation.y += 220;
-    Josh.scale.set(50, 50, 50);
-
-    Josh.children[0].material = new THREE.MeshStandardMaterial( { 
-      color: "#44f",
-      transparent: true,
-      side: THREE.DoubleSide, 
-      alphaTest: 0.5, 
-    } );
-    Josh.children[0].alphaMap = alphaMap;
-    Josh.children[0].alphaMap.magFilter = THREE.NearestFilter;
-    Josh.children[0].alphaMap.wrapT = THREE.RepeatWrapping;
-    Josh.children[0].alphaMap.repeat.y = 1;
-
-    nMax = Josh.children[0].geometry.attributes.position.count;
-
-    // clone josh
-    let foo = Josh.clone();
-    foo.children[0].material = new THREE.MeshStandardMaterial( { 
-      color: "#44f",
-      transparent: true,
-      side: THREE.DoubleSide, 
-      alphaTest: 0.5, 
-    } );
-
-    foo.position.set(0, 0, -100);
-    foo.position.x -= 2;
-    foo.rotation.y += 220;
-    foo.scale.set(250, 250, 250);
-    foo.children[0].alphaMap = alphaMap;
-    foo.children[0].alphaMap.magFilter = THREE.NearestFilter;
-    foo.children[0].alphaMap.wrapT = THREE.RepeatWrapping;
-    foo.children[0].alphaMap.repeat.y = 1;
-    foo.children[0].geometry.setDrawRange(190, 200);
-    scene.add(foo);
-    
-  }
-
-});
+}
+function render() {
+  var time = Date.now() * 0.001;
+  mesh.rotation.x = time * 0.025;
+  mesh.rotation.y = time * 0.05;
+  renderer.render( scene, camera );
+}
