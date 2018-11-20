8491f0aeafab1a0ba9c172c5e598cee959b0b4df
pfreema1 <pfreema1@gmail.com>
2018-11-19 20:37:43 -0500

    meow

--- src/index.js
+++ src/index.js
@@ -14,13 +14,16 @@ import PromisedLoad from './app/PromisedLoad';
 var container;
 var camera, scene, renderer;
 var mesh;
+var controls;
+var head;
+
 init();
 animate();
+
 function init() {
   container = document.getElementById( 'container' );
   //
   camera = new THREE.PerspectiveCamera( 27, window.innerWidth / window.innerHeight, 1, 3500 );
-  camera.position.z = 2750;
   scene = new THREE.Scene();
   scene.background = new THREE.Color( 0x050505 );
   scene.fog = new THREE.Fog( 0x050505, 2000, 3500 );
@@ -32,20 +35,29 @@ function init() {
   var light2 = new THREE.DirectionalLight( 0xffffff, 1.5 );
   light2.position.set( 0, - 1, 0 );
   scene.add( light2 );
+  // var pointLight1 = new THREE.PointLight(0xffffff);
+  // pointLight1.position.set(-2, 1, -1);
+  // scene.add(pointLight1);
   //
-  var triangles = 160000;
+  controls = new OrbitControls(camera);
+  controls.enabled = true;
+  camera.position.z = 2750;
+  controls.update();
+  //
+  var triangles = 160000;  // default: 160000
   var geometry = new THREE.BufferGeometry();
   var positions = [];
   var normals = [];
   var colors = [];
   var color = new THREE.Color();
   var n = 800, n2 = n / 2;	// triangles spread in the cube
-  var d = 12, d2 = d / 2;	// individual triangle size
+  var d = 2, d2 = d / 2;	// individual triangle size
   var pA = new THREE.Vector3();
   var pB = new THREE.Vector3();
   var pC = new THREE.Vector3();
   var cb = new THREE.Vector3();
   var ab = new THREE.Vector3();
+
   for ( var i = 0; i < triangles; i ++ ) {
     // positions
     var x = Math.random() * n - n2;
@@ -86,13 +98,27 @@ function init() {
     colors.push( color.r, color.g, color.b );
     colors.push( color.r, color.g, color.b );
   }
+
   function disposeArray() {
     this.array = null;
   }
+
   geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ).onUpload( disposeArray ) );
   geometry.addAttribute( 'normal', new THREE.Float32BufferAttribute( normals, 3 ).onUpload( disposeArray ) );
   geometry.addAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ).onUpload( disposeArray ) );
   geometry.computeBoundingSphere();
+
+  // console.log('geometry.boundingSphere:  ', geometry.boundingSphere);
+  // var sphereGeo = new THREE.SphereGeometry(688, 100, 100);
+  // var sphereMat = new THREE.MeshBasicMaterial({
+  //   color: 0xffffff,
+  //   transparent: true,
+  //   wireframe: true,
+  //   opacity: 0.1,
+  // });
+  // var sphere = new THREE.Mesh(sphereGeo, sphereMat);
+  // scene.add(sphere);
+
   var material = new THREE.MeshPhongMaterial( {
     color: 0xaaaaaa, specular: 0xffffff, shininess: 250,
     side: THREE.DoubleSide, vertexColors: THREE.VertexColors
@@ -109,7 +135,21 @@ function init() {
 
   //
   window.addEventListener( 'resize', onWindowResize, false );
+
+  
+  PromisedLoad.GetGLTF('../static/josh.glb', initHead);
 }
+
+function initHead(gltf) {
+  // console.log('gltf:  ', gltf);
+  head = gltf.scene.children[0];
+  head.scale.set(100, 100, 100);
+  console.log('head:  ', head);
+  head.rotation.set(0, 160, 0);
+  head.position.y -= 20;
+  scene.add(head);
+}
+
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
@@ -119,6 +159,7 @@ function onWindowResize() {
 function animate() {
   requestAnimationFrame( animate );
   render();
+  controls.update();
 
 }
 function render() {
