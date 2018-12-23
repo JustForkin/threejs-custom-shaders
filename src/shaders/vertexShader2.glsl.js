2fe605138540664a40994cae6253eb738dbbdb08
pfreema1 <pfreema1@gmail.com>
2018-12-23 10:34:21 -0500

    fin

--- src/shaders/vertexShader2.glsl.js
+++ src/shaders/vertexShader2.glsl.js
@@ -10,13 +10,15 @@ varying vec3 vNormal;
 varying vec3 lightVec;
 
 void main() {
-//   vNormal  = normalMatrix * normal;
-// lightVec = mat3(viewMatrix) * normalize(customPointLightPos - position); 
-  vNormal  = normalMatrix * normal;
-  // vNormal  = normal * normalMatrix;
-  lightVec = mat3(viewMatrix) * normalize(customPointLightPos - position);
- 
+  // convert normal to 'view' space
+  vNormal           = normalMatrix * normal;
+  // convert vertice position to 'view' space
+  vec4 viewPos      = modelViewMatrix * vec4(position, 1.0);
+  // convert customPointLightPos from 'world' space to 'view' space
+  vec4 viewLightPos = viewMatrix * vec4(customPointLightPos, 1.0);
+  // get the normalized vector from the vertice to the light
+  lightVec          = normalize(viewLightPos.xyz - viewPos.xyz);
 
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+  gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position,1.0);
 }
 `;
