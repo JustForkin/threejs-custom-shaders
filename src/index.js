91e3fe4df231a714460ca551c5826ff0d6cb7a6e
pfreema1 <pfreema1@gmail.com>
2018-12-23 12:25:39 -0500

    basic example

--- src/index.js
+++ src/index.js
@@ -1,252 +1,55 @@
 import 'normalize.css/normalize.css';
 import './styles/index.scss';
-import * as THREE from 'three';
-import { TweenMax, TimelineLite } from 'gsap/TweenMax';
-import OrbitControls from 'orbit-controls-es6';
-import vertexShader1 from './shaders/vertexShader1.glsl';
-import vertexShader2 from './shaders/vertexShader2.glsl';
-import fragmentShader1 from './shaders/fragmentShader1.glsl';
-import * as dat from 'dat.gui';
-// import { setupGUI } from './app/GUI';
 
-let mouse = new THREE.Vector2();
+let canvas, ctx, image;
+let offCanvas, offCtx;
 
-window.addEventListener('mousemove', onDocumentMouseMove, false);
+initCanvas();
+initOffScreenCanvas();
 
-document.addEventListener('DOMContentLoaded', () => {
-  let renderer,
-    camera,
-    scene = null;
-  const container = document.getElementById('container');
-  let controls;
-  let startTime, time;
-  let cube;
-  let rotSpeed = new THREE.Vector3(0.005, 0.003, 0.0);
-  let axesHelper;
-  let uniforms;
-  let customPointLight;
+for (let i = 0; i < canvas.width; i++) {
+  for (let j = 0; j < canvas.height; j++) {
+    let normalizedPos = {
+      x: i / canvas.width,
+      y: j / canvas.height
+    };
 
-  initialize();
-
-  // console.log('rotSpeed:  ', rotSpeed);
-  // setupGUI();
-
-  async function initialize() {
-    scene = new THREE.Scene();
-    renderer = new THREE.WebGLRenderer({
-      antialias: true // to get smoother output
-    });
-    renderer.setClearColor(0x3b3b3b);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    container.appendChild(renderer.domElement);
-
-    // create a camera in the scene
-    camera = new THREE.PerspectiveCamera(
-      35,
-      window.innerWidth / window.innerHeight,
+    let pixelData = offCtx.getImageData(
+      normalizedPos.x * image.width,
+      normalizedPos.y * image.height,
       1,
-      10000
+      1
     );
-
-    axesHelper = new THREE.AxesHelper(5);
-    scene.add(axesHelper);
-
-    addCube();
-
-    addCustomPointLight();
-
-    controls = new OrbitControls(camera);
-    scene.add(camera);
-    camera.position.z = 5;
-    controls.update();
-
-    // and then just look at it!
-    camera.lookAt(scene.position);
-    controls.update();
-
-    animate();
-  }
-
-  function addCube() {
-    /*
-      var geometry = new THREE.SphereGeometry( 5, 32, 32 );
-      var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-      var sphere = new THREE.Mesh( geometry, material );
-      scene.add( sphere );
-    */
-    // let geometry = new THREE.SphereGeometry(1, 32, 32);
-    let geometry = new THREE.BoxGeometry(1, 1, 1, 16, 16, 16);
-    uniforms = {
-      time: {
-        type: 'f',
-        value: 0
-      },
-      materialColor: {
-        type: 'v3f',
-        value: new THREE.Vector3(1.0, 0.0, 0.0)
-      },
-      ambientLightColor: {
-        type: 'v3f',
-        value: new THREE.Vector3(0.0, 0.0, 1.0)
-      },
-      ambientLightStrength: {
-        type: 'f',
-        value: 0.3
-      },
-      customPointLightPos: {
-        type: 'v3f',
-        value: new THREE.Vector3(2.0, 2.0, 2.0)
-      }
-    };
-    const shaderMaterialParams = {
-      uniforms: uniforms,
-      vertexShader: vertexShader2,
-      fragmentShader: fragmentShader1
-    };
-    const customMaterial = new THREE.ShaderMaterial(shaderMaterialParams);
-
-    cube = new THREE.Mesh(geometry, customMaterial);
-    scene.add(cube);
-  }
-
-  function addCustomPointLight() {
-    let geo = new THREE.BoxGeometry(0.3, 0.3, 0.3);
-    let mat = new THREE.MeshBasicMaterial();
-    customPointLight = new THREE.Mesh(geo, mat);
-    customPointLight.position.set(2, 2, 2);
-    scene.add(customPointLight);
-  }
-
-  function normalize(x, fromMin, fromMax) {
-    let totalRange;
-
-    x = Math.abs(x);
-    totalRange = Math.abs(fromMin) + Math.abs(fromMax);
-    // now we can map out the range from 0 to the totalRange and get a normalized (0 - 1) value
-    return x / totalRange;
-  }
-
-  function animate() {
-    requestAnimationFrame(animate);
-
-    time = performance.now() / 1000;
-
-    cube.material.uniforms.time.value = time;
-    // cube.material.uniforms.materialColor = uniforms.materialColor;
-    // cube.material.uniforms.ambientLightColor = uniforms.ambientLightColor;
-    // cube.material.uniforms.ambientLightStrength = uniforms.ambientLightStrength;
-
-    cube.rotation.x += rotSpeed.x;
-    cube.rotation.y += rotSpeed.y;
-
-    render();
-  }
-
-  function render() {
-    renderer.render(scene, camera);
-
-    controls.update();
-  }
-
-  setupGUI(rotSpeed, uniforms, cube, customPointLight);
-});
-
-function setupGUI(rotSpeed, uniforms, cube, customPointLight) {
-  let options = {
-    velx: 0,
-    vely: 0,
-    rotSpeed: rotSpeed,
-    materialColor: uniforms.materialColor.value.toArray(),
-    ambientLightColor: uniforms.ambientLightColor.value.toArray(),
-    ambientLightStrength: uniforms.ambientLightStrength.value,
-    customPointLightPos: {
-      x: 2,
-      y: 2,
-      z: 2
+    // i % 20 === 0 &&
+    //   j % 20 === 0 &&
+    //   console.log('pixelData.data:  ', pixelData.data);
+    // pixelData.data = [255, 255, 255, 255]
+    if (pixelData.data[0] > 250) {
+      ctx.fillStyle = 'rgba(0, 0, 0, 255)';
+      ctx.fillRect(i, j, 1, 1);
+    } else {
+      ctx.fillStyle = 'rgba(255, 255, 255, 255)';
+      ctx.fillRect(i, j, 1, 1);
     }
-  };
-  let gui = new dat.GUI();
-  let rotation = gui.addFolder('Rotation');
-  rotation
-    .add(options.rotSpeed, 'x', -0.02, 0.02)
-    .name('X')
-    .listen();
-  rotation
-    .add(options.rotSpeed, 'y', -0.02, 0.02)
-    .name('Y')
-    .listen();
-  rotation.open();
-  let uniformsGUI = gui.addFolder('Uniforms');
-  uniformsGUI
-    .addColor(options, 'materialColor')
-    .onChange(function(value) {
-      cube.material.uniforms.materialColor.value.x = value[0] / 255;
-      cube.material.uniforms.materialColor.value.y = value[1] / 255;
-      cube.material.uniforms.materialColor.value.z = value[2] / 255;
-    })
-    .name('materialColor')
-    .listen();
-  uniformsGUI.addColor(options, 'ambientLightColor').onChange(function(value) {
-    cube.material.uniforms.ambientLightColor.value.x = value[0] / 255;
-    cube.material.uniforms.ambientLightColor.value.y = value[1] / 255;
-    cube.material.uniforms.ambientLightColor.value.z = value[2] / 255;
-  });
-  uniformsGUI
-    .add(options, 'ambientLightStrength', 0.0, 1.0)
-    .onChange(function(value) {
-      cube.material.uniforms.ambientLightStrength.value = value;
-    });
-  uniformsGUI.open();
-
-  let customPointLightGUI = gui.addFolder('Custom Point Light');
-  customPointLightGUI
-    .add(customPointLight.position, 'x', -5, 5)
-    .onChange(function(value) {
-      cube.material.uniforms.customPointLightPos.value.x = value;
-    });
-  customPointLightGUI
-    .add(customPointLight.position, 'y', -5, 5)
-    .onChange(function(value) {
-      cube.material.uniforms.customPointLightPos.value.y = value;
-    });
-  customPointLightGUI
-    .add(customPointLight.position, 'z', -5, 5)
-    .onChange(function(value) {
-      cube.material.uniforms.customPointLightPos.value.z = value;
-    });
-  customPointLightGUI.open();
-
-  let box = gui.addFolder('Cube');
-  box
-    .add(cube.scale, 'x', 0, 3)
-    .name('Width')
-    .listen();
-  box
-    .add(cube.scale, 'y', 0, 3)
-    .name('Height')
-    .listen();
-  box
-    .add(cube.scale, 'z', 0, 3)
-    .name('Length')
-    .listen();
-  box
-    .add(cube.position, 'x', -2, 2)
-    .name('X')
-    .listen();
-  box
-    .add(cube.position, 'y', -2, 2)
-    .name('Y')
-    .listen();
-  box
-    .add(cube.position, 'z', -2, 2)
-    .name('Z')
-    .listen();
-  box.add(cube.material, 'wireframe').listen();
-  box.open();
+  }
 }
 
-function onDocumentMouseMove(event) {
-  event.preventDefault();
-  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+function initCanvas() {
+  canvas = document.createElement('canvas');
+  ctx = canvas.getContext('2d');
+  image = document.getElementById('textTexture');
+  canvas.id = 'canvas';
+  document.body.appendChild(canvas);
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
 }
+
+function initOffScreenCanvas() {
+  offCanvas = document.createElement('canvas');
+  offCtx = offCanvas.getContext('2d');
+  offCanvas.width = image.width;
+  offCanvas.height = image.height;
+  offCtx.drawImage(image, 0, 0, image.width, image.height);
+}
+
+// ctx.drawImage(image, 100, 100);
